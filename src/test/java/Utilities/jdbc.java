a28677885cd4e3f71eddc15b44384026a5114109
blossomnell <blossom83@gmail.com>
2025-09-23 14:30:25 -0500

    code cleanup

--- src/test/java/Utilities/jdbc.java
+++ src/test/java/Utilities/jdbc.java
@@ -3,9 +3,9 @@ package Utilities;
 import java.sql.*;
 
 public class jdbc {
-	private static final String URL = "jdbc:postgresql://localhost:5432/RecipeScraping";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "Connect@1406";
+	private static final String URL = "jdbc:postgresql://localhost:5432/lfv_recipes";
+    private static final String USER = "blossom";
+    private static final String PASSWORD = "recipe_scraping";
     
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
